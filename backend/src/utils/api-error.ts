3cf674e906cd821f4c68f5330303df06f3a63261
Hugo Roca <hugo.rock20@hotmail.com>
2020-09-14 22:34:33 -0500

    edit api-error

--- backend/src/utils/api-error.ts
+++ backend/src/utils/api-error.ts
@@ -1,8 +1,5 @@
 import { Context } from "koa";
 import ResponseError from "./response-error";
-import LogManager from "./log-manager";
-
-const logger = new LogManager();
 
 export default async (ctx: Context, next: () => Promise<any>) => {
   try {
@@ -11,18 +8,6 @@ export default async (ctx: Context, next: () => Promise<any>) => {
       return ResponseError.notFound(ctx);
     }
   } catch (err) {
-    const { id = 0, country = "CC" } = ctx.state.application || {};
-    const request = ctx.request.body || {};
-    logger.logError(
-      err.class || "",
-      err.method || "",
-      request,
-      err.message,
-      country,
-      err.exception,
-      err.code_transaction || "",
-      id
-    );
     return ResponseError.internalServerError(ctx, err);
   }
 };
