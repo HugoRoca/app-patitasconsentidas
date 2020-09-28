db2ea9d0cbe60fcb10055bb2758a64f9f80d1362
Hugo Roca <hugo.rock20@hotmail.com>
2020-09-27 22:32:19 -0500

    pendiente koa-multer

--- src/routes/product.route.ts
+++ src/routes/product.route.ts
@@ -1,9 +1,13 @@
 import Router from "koa-router"
+import compose from "koa-compose"
+import multer from "multer"
 import ProductController from "../controllers/product.controller"
+import { storage, fileFilter } from "../utils/helpers"
 
 const router = new Router({ prefix: "/products" })
 const controller = new ProductController()
+const upload = multer({ storage, fileFilter }).single("image")
 
-router.post("/", controller.create)
+router.post("/", upload, controller.create)
 
 export default router
